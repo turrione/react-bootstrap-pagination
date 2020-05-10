86c63463312344bdcc879d933adac5674d98a197
Marc Torrents <torrentsmarc@gmail.com>
2020-05-10 03:21:36 +0200

    delete styles and add PageItem component

--- src/index.js
+++ src/index.js
@@ -1,48 +1,26 @@
 import React from 'react';
 import { getPagination } from './utils/getPagination';
-
-
-//Styles
-const styles = {
-    pagination: {
-        display: 'flex',
-        paddingLeft: 0,
-        listStyle: 'none',
-        margin: '20px 0',
-        borderRadius: '.25rem'
-    },
-    page_item: {
-        position: 'relative',
-        display: 'block',
-        padding: '.5rem .75rem',
-        lineHeight: 1.25,
-        color: '#007bff',
-        backgroundColor: '#fff',
-        border: '1px solid #dee2e6'
-    }
-}
+import PageItem from './PageItem';
 
 export default class Pagination extends React.Component {
 
     render() {
-        console.log(this.props);
+
         const { onClick, size, ariaLabel } = this.props;
         const pagination = getPagination(this.props);
-        console.log({ pagination });
 
         return (
             <nav aria-label={ariaLabel}>
                 <ul className={`pagination justify-content-center ${(size === 'sm' || size === 'lg') && 'pagination-' + size}`}>
                     {
                         pagination.map((page, i) =>
-                            <li key={`${page.page}-${i}`} className={`page-item ${page.class && page.class}`} >
-                                <a
-                                    className={'page-link'}
-                                    onClick={() => onClick && onClick(page.page)}
-                                    {...onClick ? { href: '#' } : { href: page.href }} >
-                                    {page.text}
-                                </a>
-                            </li>
+                            <PageItem
+                                key={`${page}-${i}`}
+                                text={page.text}
+                                page={page.page}
+                                className={page.class}
+                                href={page.href}
+                                onClick={onClick} />
                         )
                     }
                 </ul>
@@ -73,12 +51,14 @@ Pagination.propTypes = {
 };
 
 Pagination.defaultProps = {
+    currentPage: 1,
     ariaLabel: 'Page navigator',
     activeClass: 'active',
     disabledClass: 'disabled',
     showMax: 5,
     center: true,
     size: 'md', // sm md lg
-    prevText: 'Previous',
-    nextText: 'Next'
+    prevNext: true,
+    prevText: '⟨',
+    nextText: '⟩'
 }
